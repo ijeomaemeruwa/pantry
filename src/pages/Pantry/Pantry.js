cca4f0791aeb47690671b4d7a2875d4a7e0cfa16
ijeomaemeruwa <helloijeoma@gmail.com>
2020-11-23 14:09:48 +0000

    styled pantry page

--- src/pages/Pantry/Pantry.js
+++ src/pages/Pantry/Pantry.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import './pantry.scss'
 import { db } from '../../firebase/config'
 import AddRecipe from '../../components/Modals/AddRecipeModal/AddRecipe'
 import Menu from '../../components/Menu/Menu'
@@ -6,6 +7,8 @@ import Menu from '../../components/Menu/Menu'
 import Card from 'react-bootstrap/Card'
 // import PantryDetails from '../../components/Modals/PantryDetails/PantryDetails';
 import { GrAddCircle } from 'react-icons/gr'
+import { AiOutlineDelete } from 'react-icons/ai'
+import { RiShareForwardLine } from 'react-icons/ri'
 
 
 const Pantry = (props) => {
@@ -26,6 +29,7 @@ const Pantry = (props) => {
       };
     }, []);
 
+
     const deleteRecipeCard = id => {
       db.collection('pantry')
       .doc(id)
@@ -37,19 +41,22 @@ const Pantry = (props) => {
     <>
     <Menu />
     <div>
-    <div>
+
+    <div className="pantry_header mx-auto text-center">
     <button  
     className="add_btn"
     onClick={() => setModalShow(true)}>
      Add Recipe <GrAddCircle/>
     </button> 
     <AddRecipe show={modalShow} onHide={() => setModalShow(false)} />
+    {/* <h4>Your Pantry</h4> */}
+    </div>
 
-    <h4>Your Pantry</h4>
-    <div className="card_container">
+    <div className="card_container container">
+    {/* Click on card to view recipe modal */}
       {
         recipeCard.map(recipe => (
-        <Card key={recipe.id} style={{ width: '16rem' }}>
+        <Card key={recipe.id} style={{ width: '16rem' }}> 
          <Card.Img variant="top" src="holder.js/100px180" />
          <Card.Body>
          <Card.Title><h5>{recipe.title}</h5></Card.Title>
@@ -57,14 +64,13 @@ const Pantry = (props) => {
           <small>{recipe.category}</small>
          </Card.Text>
          <div className="card_btn">
-         <button>Icon</button>
-         <button onClick={() => deleteRecipeCard.id}>Icon</button>
+         <button onClick={() => deleteRecipeCard.id}><AiOutlineDelete /></button>
+         <button><RiShareForwardLine /></button>
          </div>
         </Card.Body>
         </Card>
         ))
       }
-    </div>
     </div>
     </div> 
     </>
