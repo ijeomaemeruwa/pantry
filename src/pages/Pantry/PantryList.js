12bc276c2bd66c54178198d2be9508d22b81af40
ijeomaemeruwa <helloijeoma@gmail.com>
2020-11-27 03:30:12 +0000

    updates to pantry list

--- src/pages/Pantry/PantryList.js
+++ src/pages/Pantry/PantryList.js
@@ -1,69 +1,64 @@
 
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import './pantry.scss'
-import { db } from '../../firebase/config'
+//import { db } from '../../firebase/config'
 // import CustomModal from '../../components/Modals/CustomModal/CustomModal'
 
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
 import { AiOutlineDelete } from 'react-icons/ai'
 import { RiShareForwardLine } from 'react-icons/ri'
+import useDatabase from '../../customHook/useDatabase'
 
 const PantryList = () => {
-    const [recipeCard, setRecipeCard] = useState([]);
-    // const [show, setShow] = useState(false);
-    // const [item, setItem] = useState(false);
+    //const [recipeCard, setRecipeCard] = useState([]);
+    const { docs } = useDatabase('pantry');
 
-    // const handleClose = () => setShow(false);
-    // const handleShow = (id) => {
-    //     setItem(recipeCard[id])
-    //     setShow(true)
-    // }
-
-    useEffect(() => {
-        const unsubscribe = db.collection('pantry').onSnapshot(snapshot => {
-        const allRecipes = snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
-         setRecipeCard(allRecipes)
-        });
-        return () => {
-          console.log('cleanup');
-          unsubscribe();
-        };
-      }, []);
+    // useEffect(() => {
+    //     const unsubscribe = db.collection('pantry').onSnapshot(snapshot => {
+    //     const allRecipes = snapshot.docs.map(doc => ({
+    //       id: doc.id,
+    //       ...doc.data()
+    //     }));
+    //      setRecipeCard(allRecipes)
+    //     });
+    //     return () => {
+    //       console.log('cleanup');
+    //       unsubscribe();
+    //     };
+    //   }, []);
   
   
-      const deleteRecipeCard = e => {
-        // db.collection('pantry')
-        // .doc(id)
-        // .delete();
+      // const deleteRecipeCard = e => {
+      //   // db.collection('pantry')
+      //   // .doc(id)
+      //   // .delete();
 
-        if(e.target.tagName === 'svg'){
-          const id = e.target.getAttribute('id');
-          db.collection('pantry').doc(id).delete();
-      }
-      };
+      //   if(e.target.tagName === 'svg'){
+      //     const id = e.target.getAttribute('id');
+      //     db.collection('pantry').doc(id).delete();
+      // }
+      // };
       
       
     return (
     <>
       <div className="card_container container">
-      {/* <CustomModal size="lg" item={item} show={show} handleClose={handleClose} /> */}
       {
-        recipeCard.map(recipe => (
+        docs && docs.map(recipe => (
         <Card key={recipe.id} style={{ width: '16rem' }} className="card"> 
     
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={recipe.url} alt={recipe.title} />
          <Card.Body>
          <Card.Title><h5>{recipe.title}</h5></Card.Title>
          <Card.Text>
           <small>{recipe.category}</small>
          </Card.Text>
+
          <Link to={{ pathname: `/pantrydetails/${recipe.id}`, state: { recipe: recipe.title } }}>View</Link>
+
          <div className="card_btn">
-         <button onClick={() => deleteRecipeCard.id}><AiOutlineDelete /></button>
+         <button><AiOutlineDelete /></button>
          <button><RiShareForwardLine /></button>
          </div>
         </Card.Body>
