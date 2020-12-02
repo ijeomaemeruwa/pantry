db63f5a7b343cb15b1c5e867e93054a33c935924
ijeomaemeruwa <helloijeoma@gmail.com>
2020-12-02 14:13:13 +0000

    firebase database configuration

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,19 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxpiygaES-G9sw3jnOczvJcMy8i52cSfQ",
-    authDomain: "pantry-recipe-app.firebaseapp.com",
-    databaseURL: "https://pantry-recipe-app.firebaseio.com",
-    projectId: "pantry-recipe-app",
-    storageBucket: "pantry-recipe-app.appspot.com",
-    messagingSenderId: "717897658061",
-    appId: "1:717897658061:web:caf17b8d997f2ce5084384",
-    measurementId: "G-LHVZ9RX9XM"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STRORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export const db = firebase.firestore();
-export const storage = firebase.storage()
+export const database = firebase.firestore();
 
